9cdb9d893e6655529abb0fdc0c2c56b149647cc4
chenyu <chenyu@huaaotech.com>
2017-07-05 10:33:05 +0800

    修改BaseRecyclerViewAdapterHelper的依赖问题

--- app/src/main/java/com/future/association/community/CommunityFragment.java
+++ app/src/main/java/com/future/association/community/CommunityFragment.java
@@ -2,18 +2,18 @@ package com.future.association.community;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.future.association.R;
+import com.future.baselib.activity.BaseFragment;
 
 /**
  * 社区Fragment
  * A simple {@link Fragment} subclass.
  */
-public class CommunityFragment extends Fragment {
+public class CommunityFragment extends BaseFragment {
 
 
     public CommunityFragment() {
@@ -22,10 +22,20 @@ public class CommunityFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false);
+    protected void getBundleExtras(Bundle extras) {
+
+    }
+
+    @Nullable
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_community;
+    }
+
+
+    @Override
+    protected void initView(View view, @Nullable Bundle savedInstanceState) {
+
     }
 
 }
