e75730e036fa0e61aff2b830aef93c5a1841f639
chenyu <89396962@qq.com>
2017-07-08 11:43:19 +0800

    监督页面完成

--- app/src/main/java/com/future/association/supervice/SuperviceFragment.java
+++ app/src/main/java/com/future/association/supervice/SuperviceFragment.java
@@ -1,6 +1,7 @@
 package com.future.association.supervice;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,8 +10,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.future.association.R;
 import com.future.baselib.activity.BaseFragment;
+import com.future.baselib.utils.CommonUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,7 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class SuperviceFragment extends BaseFragment {
+
 
 
     public SuperviceFragment() {
@@ -43,6 +48,7 @@ public class SuperviceFragment extends BaseFragment {
         RecyclerView superviceRv = (RecyclerView) view.findViewById(R.id.supervice_rv);
         superviceRv.setLayoutManager(new LinearLayoutManager(getActivity()));
         SuperviceAdapter superviceAdapter = new SuperviceAdapter(R.layout.supervice_item,null);
+        superviceRv.addOnItemTouchListener(detailListener);
         View head = getHead();
         superviceAdapter.addHeaderView(head,0);
         //TODO 测试
@@ -60,6 +66,7 @@ public class SuperviceFragment extends BaseFragment {
         RecyclerView headRv = (RecyclerView) view.findViewById(R.id.supervice_head_rv);
         headRv.setLayoutManager(new GridLayoutManager(getActivity(),3));
         SuperviceHeadAdapter adapter = new SuperviceHeadAdapter(R.layout.supervice_head_item,null);
+        headRv.addOnItemTouchListener(applyListener);
         List<String> test = new ArrayList<>();
         //TODO 测试
         for (int i = 0; i < 9; i++) {
@@ -69,5 +76,25 @@ public class SuperviceFragment extends BaseFragment {
         headRv.setAdapter(adapter);
         return view;
     }
+    private RecyclerView.OnItemTouchListener detailListener = new OnItemClickListener() {
+        @Override
+        public void onSimpleItemClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
+            //跳往详情页面
+            if (!CommonUtils.isFastDoubleClick()){
+                Intent intent = new Intent(getActivity(),SuperviceDetailActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 
+    private RecyclerView.OnItemTouchListener applyListener = new OnItemClickListener() {
+        @Override
+        public void onSimpleItemClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
+            //跳往申请页面
+            if (!CommonUtils.isFastDoubleClick()){
+                Intent intent = new Intent(getActivity(),SuperviceApplyActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
