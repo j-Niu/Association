1a7da23db03eea2ea369927b4f4fbeee8139fa2c
niexueliang <code_nil@163.com>
2017-07-15 12:12:52 +0800

    Merge branch 'master' of https://github.com/j-Niu/Association
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    隐藏输入法键盘

--- app/src/main/java/com/future/association/login/PerfectInformationActivity.java
+++ app/src/main/java/com/future/association/login/PerfectInformationActivity.java
@@ -1,9 +1,14 @@
 package com.future.association.login;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.content.ContextCompat;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.future.association.BR;
 import com.future.association.R;
@@ -61,5 +66,42 @@ public class PerfectInformationActivity extends BaseActivity {
 
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        //Finger touch screen event
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            // get current focus,Generally it is EditText
+            View view = getCurrentFocus();
+            if (isShouldHideSoftKeyBoard(view, ev)) {
+                hideSoftKeyBoard(view.getWindowToken());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 
+    private boolean isShouldHideSoftKeyBoard(View view, MotionEvent event) {
+        if (view != null && (view instanceof EditText)) {
+            int[] l = {0, 0};
+            view.getLocationInWindow(l);
+            int left = l[0], top = l[1], bottom = top + view.getHeight(), right = left
+                    + view.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // If click the EditText event ,ignore it
+                return false;
+            } else {
+                return true;
+            }
+        }
+        // if the focus is EditText,ignore it;
+        return false;
+    }
+
+    private void hideSoftKeyBoard(IBinder token) {
+        if (token != null) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(token,
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
 }
