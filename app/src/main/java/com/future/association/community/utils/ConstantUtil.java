f33dc3b87ae5ce2e7d36f9c32f108452162cc083
luojianfei <Leo_jf@163.com>
2017-07-17 11:13:44 +0800

    完善社区版块功能

--- app/src/main/java/com/future/association/community/utils/ConstantUtil.java
+++ app/src/main/java/com/future/association/community/utils/ConstantUtil.java
@@ -5,7 +5,6 @@ package com.future.association.community.utils;
  */
 public class ConstantUtil {
 
-    public static final String DB_NAME = "user.db" ;
     public static final int DOUBLE_CLICK_EXIT_DENY = 2000 ;//双击间隔500ms以内退出应用
     public static final String PAGE_SIZE = "20" ;//数据列表每次加载数据条数
 
