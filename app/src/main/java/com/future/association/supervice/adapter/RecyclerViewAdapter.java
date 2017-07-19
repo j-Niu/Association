dea3b445ecb64a01f99d4024d6fa5b9330d441c9
chenyu <89396962@qq.com>
2017-07-19 23:45:13 +0800

    增加图片选择框架

--- app/src/main/java/com/future/association/supervice/adapter/RecyclerViewAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.future.association.supervice.adapter;
-
-/**
- * Created by rain on 2017/7/12.
- */
-
-public class RecyclerViewAdapter {
-
-}
--- app/src/main/java/com/future/association/supervice/viewmodel/SuperviceApplyViewModel.java
+++ app/src/main/java/com/future/association/supervice/viewmodel/SuperviceApplyViewModel.java
@@ -3,6 +3,7 @@ package com.future.association.supervice.viewmodel;
 import android.content.Intent;
 import android.databinding.ObservableField;
 import android.graphics.Color;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.View;
 
 import com.bigkoo.pickerview.OptionsPickerView;
@@ -11,14 +12,18 @@ import com.future.association.community.utils.TextUtil;
 import com.future.association.databinding.ActivitySuperviceApplyBinding;
 import com.future.association.login.bean.GetJsonDataUtil;
 import com.future.association.login.bean.JsonBean;
+import com.future.association.supervice.FullyGridLayoutManager;
 import com.future.association.supervice.SupericeApi;
+import com.future.association.supervice.adapter.GridImageAdapter;
 import com.future.association.supervice.model.SupericeDetail;
 import com.future.association.supervice.view.SuperviceApplyActivity;
+import com.future.baselib.utils.CommonUtils;
 import com.future.baselib.utils.HttpRequest;
 import com.future.baselib.utils.ToastUtils;
 import com.google.gson.Gson;
 import com.jakewharton.rxbinding2.view.RxView;
 import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.compress.Luban;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
@@ -50,6 +55,9 @@ public class SuperviceApplyViewModel {
     private ArrayList<ArrayList<String>> options2Items = new ArrayList<>();
     private ArrayList<ArrayList<ArrayList<String>>> options3Items = new ArrayList<>();
     private final SupericeDetail supericeDetailBean = new SupericeDetail();
+    private GridImageAdapter adapter;
+    private int maxSelectNum = 5;
+
 
     public SuperviceApplyViewModel(SuperviceApplyActivity activity, ActivitySuperviceApplyBinding binding, String type) {
         this.activity = activity;
@@ -60,6 +68,28 @@ public class SuperviceApplyViewModel {
     }
 
     private void initView() {
+        mBindIng.imgRv.setLayoutManager(new FullyGridLayoutManager(activity, 4, GridLayoutManager.VERTICAL, false));
+        adapter = new GridImageAdapter(activity, onAddPicClickListener);
+        adapter.setList(selectList);
+        adapter.setSelectMax(maxSelectNum);
+        mBindIng.imgRv.setAdapter(adapter);
+        adapter.setOnItemClickListener(new GridImageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                if (selectList.size() > 0) {
+                    LocalMedia media = selectList.get(position);
+                    String pictureType = media.getPictureType();
+                    int mediaType = PictureMimeType.pictureToVideo(pictureType);
+                    switch (mediaType) {
+                        case 1:
+                            // 预览图片 可自定长按保存路径
+                            //PictureSelector.create(MainActivity.this).externalPicturePreview(position, "/custom_file", selectList);
+                            PictureSelector.create(activity).externalPicturePreview(position, selectList);
+                            break;
+                    }
+                }
+            }
+        });
         RxView.clicks(mBindIng.submitBtn)
                 .throttleFirst(1, java.util.concurrent.TimeUnit.SECONDS)
                 .subscribe(new Consumer<Object>() {
@@ -67,17 +97,16 @@ public class SuperviceApplyViewModel {
                     public void accept(@NonNull Object o) throws Exception {
                         if (canCommit()) {
                             SupericeDetail supericeDetail = SuperviceApplyViewModel.this.supericeDetail.get();
-                            String[] imgs = supericeDetail.getImage();
                             StringBuffer buffer = new StringBuffer();
-                            //TODO 测试
-                            buffer.append("http://img5.iqilu.com/c/u/2012/1016/1350352494909.jpg,http://img5.iqilu.com/c/u/2012/1016/1350352494909.jpg");
-//                            for (int i = 0; i < imgs.length; i++) {
-//                                if (i == 0) {
-//                                    buffer.append(imgs[0]);
-//                                } else {
-//                                    buffer.append("," + imgs[i]);
-//                                }
-//                            }
+                            for (int i = 0; i < selectList.size(); i++) {
+                                if (i == 0) {
+                                    String img = CommonUtils.getImg(selectList.get(0).getCompressPath());
+                                    buffer.append(img);
+                                } else {
+                                    String img = CommonUtils.getImg(selectList.get(i).getCompressPath());
+                                    buffer.append("," + img);
+                                }
+                            }
                             SupericeApi.getInstance().publishSuperice(activity, MyApp.getUserToken(), supericeDetail.getType(),
                                     supericeDetail.getAddress(), supericeDetail.getTitle(), supericeDetail.getReason(), buffer.toString())
                                     .setListener(new HttpRequest.OnNetworkListener<SupericeDetail>() {
@@ -112,19 +141,40 @@ public class SuperviceApplyViewModel {
                     }
                 });
 
-       RxView.clicks(mBindIng.addImgTv)
-               .throttleFirst(1,TimeUnit.SECONDS)
-               .subscribe(new Consumer<Object>() {
-                   @Override
-                   public void accept(@NonNull Object o) throws Exception {
-//                       PictureSelector.create(SuperviceApplyActivity.class).c
-                       PictureSelector.create(activity)
-                               .openGallery(PictureMimeType.ofImage())
-                               .forResult(PictureConfig.CHOOSE_REQUEST);
-                   }
-               });
 
     }
+
+    private List<LocalMedia> selectList;
+    private GridImageAdapter.onAddPicClickListener onAddPicClickListener = new GridImageAdapter.onAddPicClickListener() {
+        @Override
+        public void onAddPicClick() {
+            PictureSelector.create(activity)
+                    .openGallery(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                    .maxSelectNum(maxSelectNum)// 最大图片选择数量
+                    .minSelectNum(1)// 最小选择数量
+                    .imageSpanCount(4)// 每行显示个数
+                    .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选
+                    .previewImage(true)// 是否可预览图片
+                    .compressGrade(Luban.THIRD_GEAR)// luban压缩档次，默认3档 Luban.FIRST_GEAR、Luban.CUSTOM_GEAR
+                    .isCamera(true)// 是否显示拍照按钮
+                    .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                    //.setOutputCameraPath("/CustomPath")// 自定义拍照保存路径
+//                    .enableCrop(true)// 是否裁剪
+                    .compress(true)// 是否压缩
+                    .compressMode(PictureConfig.LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
+                    //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                    .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+//                    .withAspectRatio(aspect_ratio_x, aspect_ratio_y)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+//                    .hideBottomControls(cb_hide.isChecked() ? false : true)// 是否显示uCrop工具栏，默认不显示
+                    .isGif(false)// 是否显示gif图片
+//                    .freeStyleCropEnabled(cb_styleCrop.isChecked())// 裁剪框是否可拖拽
+//                    .showCropFrame(cb_showCropFrame.isChecked())// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+//                    .showCropGrid(cb_showCropGrid.isChecked())// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+//                    .openClickSound(cb_voice.isChecked())// 是否开启点击声音
+                    .selectionMedia(selectList)// 是否传入已选图片
+                    .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+        }
+    };
 
     private boolean canCommit() {
         SupericeDetail info = supericeDetail.get();
@@ -144,9 +194,9 @@ public class SuperviceApplyViewModel {
             ToastUtils.shortToast(activity, "请填写监督事由");
             return false;
         }
-//        if (supericeDetail.getImage() == null || supericeDetail.getImage().length == 0) {
-//            ToastUtils.shortToast(activity, "请至少上传一张图片");
-//        }
+        if (selectList == null || selectList.size() == 0) {
+            ToastUtils.shortToast(activity, "请至少上传一张图片");
+        }
 
         return true;
     }
@@ -262,14 +312,14 @@ public class SuperviceApplyViewModel {
             switch (requestCode) {
                 case PictureConfig.CHOOSE_REQUEST:
                     // 图片选择结果回调
-                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    selectList = PictureSelector.obtainMultipleResult(data);
                     // 例如 LocalMedia 里面返回三种path
                     // 1.media.getPath(); 为原图path
                     // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
                     // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
                     // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
-//                    adapter.setList(selectList);
-//                    adapter.notifyDataSetChanged();
+                    adapter.setList(selectList);
+                    adapter.notifyDataSetChanged();
                     DebugUtil.i("onActivityResult:", "onActivityResult:" + selectList.toString());
                     break;
             }
