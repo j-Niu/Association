9cbafec7bdbf3b01517c805a6a036e0af50b58fc
javakam <1772664927@qq.com>
2017-07-23 17:41:19 +0800

    Part My   .   Net for Data  .

---
+++ app/src/main/java/com/future/association/personal/PersonConstant.java
@@ -0,0 +1,14 @@
+package com.future.association.personal;
+
+/**
+ * Created by javakam on 2017/7/23.
+ */
+public class PersonConstant {
+    public static final String PAGE_SIZE_DEFAULT = "20" ;//数据列表每次加载数据条数
+
+
+
+    public static final String MY_INFO_SHOW = "_mymsg_001" ;//我的信息显示
+
+
+}
