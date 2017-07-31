eaf3b68e1f2b09ff378ae26d24dcd2e86430dcab
chenyu <89396962@qq.com>
2017-07-31 21:33:07 +0800

    bug修改

--- app/src/main/java/com/future/association/supervice/adapter/SuperviceAdapter.java
+++ app/src/main/java/com/future/association/supervice/adapter/SuperviceAdapter.java
@@ -1,14 +1,19 @@
 package com.future.association.supervice.adapter;
 
+import android.graphics.drawable.Drawable;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.future.association.R;
-import com.future.association.common.GlideUtils;
 import com.future.association.supervice.model.SupericeList;
 
 import java.io.File;
@@ -25,11 +30,25 @@ public class SuperviceAdapter extends BaseQuickAdapter<SupericeList.SupericeList
 
     @Override
     protected void convert(BaseViewHolder baseViewHolder, SupericeList.SupericeListInfo item) {
-        ImageView imageView = baseViewHolder.getView(R.id.iv);
+        final ImageView imageView = baseViewHolder.getView(R.id.iv);
         String image = item.getImage();
         image = image.replace("\\/", File.separator);
         Glide.with(mContext).load(image)
-                .apply(GlideUtils.defaultImg())
+                .listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object o, Target<Drawable> target, boolean b) {
+                imageView.setVisibility(View.GONE);
+                return true;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable drawable, Object o, Target<Drawable> target, DataSource dataSource, boolean b) {
+                imageView.setVisibility(View.VISIBLE);
+                return false;
+            }
+
+
+        })
                 .into(imageView);
         baseViewHolder.setText(R.id.title,item.getTitle())
                 .setText(R.id.time,item.getTime());
