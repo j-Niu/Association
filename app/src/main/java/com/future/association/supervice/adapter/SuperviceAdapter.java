b04c57fe0bb8b4f21053adc4a39c2c534d3260a0
chenyu <89396962@qq.com>
2017-08-06 23:25:34 +0800

    解决监督列表图片的bug

--- app/src/main/java/com/future/association/supervice/adapter/SuperviceAdapter.java
+++ app/src/main/java/com/future/association/supervice/adapter/SuperviceAdapter.java
@@ -1,19 +1,16 @@
 package com.future.association.supervice.adapter;
 
-import android.graphics.drawable.Drawable;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.future.association.R;
+import com.future.association.common.GlideUtils;
+import com.future.association.community.utils.TextUtil;
 import com.future.association.supervice.model.SupericeList;
 
 import java.io.File;
@@ -23,7 +20,7 @@ import java.util.List;
  * Created by rain on 2017/7/7.
  */
 
-public class SuperviceAdapter extends BaseQuickAdapter<SupericeList.SupericeListInfo,BaseViewHolder> {
+public class SuperviceAdapter extends BaseQuickAdapter<SupericeList.SupericeListInfo, BaseViewHolder> {
     public SuperviceAdapter(@LayoutRes int layoutResId, @Nullable List<SupericeList.SupericeListInfo> data) {
         super(layoutResId, data);
     }
@@ -33,24 +30,16 @@ public class SuperviceAdapter extends BaseQuickAdapter<SupericeList.SupericeList
         final ImageView imageView = baseViewHolder.getView(R.id.iv);
         String image = item.getImage();
         image = image.replace("\\/", File.separator);
-        Glide.with(mContext).load(image)
-                .listener(new RequestListener<Drawable>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object o, Target<Drawable> target, boolean b) {
-                imageView.setVisibility(View.GONE);
-                return true;
-            }
+        if (!TextUtil.isEmpty(image) && image.startsWith("http")) {
+            imageView.setVisibility(View.VISIBLE);
+            Glide.with(mContext).load(image)
+                    .apply(GlideUtils.defaultImg())
+                    .into(imageView);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
 
-            @Override
-            public boolean onResourceReady(Drawable drawable, Object o, Target<Drawable> target, DataSource dataSource, boolean b) {
-                imageView.setVisibility(View.VISIBLE);
-                return false;
-            }
-
-
-        })
-                .into(imageView);
-        baseViewHolder.setText(R.id.title,item.getTitle())
-                .setText(R.id.time,item.getTime());
+        baseViewHolder.setText(R.id.title, item.getTitle())
+                .setText(R.id.time, item.getTime());
     }
 }
