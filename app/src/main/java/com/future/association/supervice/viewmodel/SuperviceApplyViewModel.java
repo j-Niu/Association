7d3b8a7d48f4d2efef02858a2980416c199a0d44
chenyu <chenyu@huaaotech.com>
2017-08-18 10:19:04 +0800

    修改监督发布地址逗号隔开的bug

--- app/src/main/java/com/future/association/supervice/viewmodel/SuperviceApplyViewModel.java
+++ app/src/main/java/com/future/association/supervice/viewmodel/SuperviceApplyViewModel.java
@@ -241,8 +241,8 @@ public class SuperviceApplyViewModel {
             @Override
             public void onOptionsSelect(int options1, int options2, int options3, View v) {
                 //返回的分别是三个级别的选中位置
-                String tx = options1Items.get(options1).getPickerViewText() +
-                        options2Items.get(options1).get(options2) +
+                String tx = options1Items.get(options1).getPickerViewText() + ","+
+                        options2Items.get(options1).get(options2) + "," +
                         options3Items.get(options1).get(options2).get(options3);
                 supericeDetailBean.setAddress(tx);
                 mBindIng.address.setText(tx);
